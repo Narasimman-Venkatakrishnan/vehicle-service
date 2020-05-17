5e82fd96ba43ad1046ab890d914b701295c21ba7
venkatl <Savi@2020>
2020-05-17 18:27:39 +0200

    UnAuthorizedException

---
+++ src/main/java/com/warehouse/exception/UnAuthorizedException.java
@@ -0,0 +1,12 @@
+package com.warehouse.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+public class UnAuthorizedException extends RuntimeException {
+    public UnAuthorizedException(String message) {
+        super(message);
+    }
+}
+
